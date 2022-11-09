35af039b9845c6db905c22ab2b78ab40f761635c
Tylerdays <tylerdday.software@gmail.com>
2022-11-09 12:53:40 -0700

    created the dir and finished my first function!

---
+++ jav.js
@@ -0,0 +1,4 @@
+//this file is my testing ground before putting stuff in the main file
+
+function stringCount(letsCount);
+    let letscount =
---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+//first function of two integers times five
+
+
+function timesFive(num1, num2){
+    let result = (num1 + num2) * 5;
+    return result;
+}
+//Console log for the timeFive function
+console.log(timesFive(10,10));
+
+
+
+
+
+    
