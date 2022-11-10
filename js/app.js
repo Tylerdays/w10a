f59559bf697939ee7be92b0260717e817a0c119f
Tylerdays <tylerdday.software@gmail.com>
2022-11-09 18:50:12 -0700

    Final assembled assignment

--- js/app.js
+++ js/app.js
@@ -26,7 +26,26 @@ console.log(stringCount("This"))
 console.log(stringCount("how many characters"))
 console.log(stringCount("Be"))
 
+//Array of strings
 
+
+function phFinder(finalPH){
+    
+    //for loop for running through the string
+        for (let i = 0; i < finalPH.length; i++) {
+    
+            let phFind = finalPH[i];//  To check the index's of the arrays
+            let searchPh = phFind.startsWith('ph'); //To find the instances of ph and tie the vars together
+            //if statement to find ph
+            if (searchPh){
+            }  return finalPH[i];
+        }
+            
+    }
+    //I've gotten it to print to console, but couldn't figure out how to make it print the specific element of ph
+    console.log (phFinder(["bang","boom","phem","snake","food"]));
+    console.log (phFinder(["yak","photo","god","good","goad"]));
+    console.log (phFinder(["phone","fish","phog","ham","cheese"]));
 
 
     
